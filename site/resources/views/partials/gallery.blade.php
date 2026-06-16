9593862bf1d515ac8c9b54b129a0d6784d7543b3
pharelouindou <ayeyemiouindou@gmail.com>
2026-06-16 09:21:07 +0100

    remove everything

--- site/resources/views/partials/gallery.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="gallery-grid">
-    @foreach (config('site.gallery') as $photo)
-        <figure class="gallery-item">
-            <img src="{{ asset($photo['file']) }}" alt="{{ $photo['caption'] }}" loading="lazy" />
-            <figcaption>{{ $photo['caption'] }}</figcaption>
-        </figure>
-    @endforeach
-</div>
